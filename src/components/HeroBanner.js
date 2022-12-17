df3156970134a6ec59c76992c2e95452e6e75316
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-17 19:30:42 +0530

    fetched data and made page scrollable

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -7,8 +7,7 @@ const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
+      Sweat, Smile <br /> And Repeat
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
-import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
@@ -13,8 +12,7 @@ const Home = () => {
     <Box>
       <HeroBanner />
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
-    </Box>
+</Box>
   );
 };
 
